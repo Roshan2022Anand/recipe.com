9bbaf9b92716591b116d424f0d3481440713625a
unknown <roshananand2005@gmail.com>
2024-08-27 13:00:03 +0530

    fixed isseue

--- app/FavItem/page.js
+++ app/FavItem/page.js
@@ -1,10 +1,17 @@
 "use client"
-import React, {useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import Menu from '@/Components/Menu'
 import RecipeCard from '@/Components/RecipeCard'
 const page = () => {
-    const [favItemList, setfavItemList] = useState(JSON.parse(localStorage.getItem("favItemArr")))
+    const [favItemList, setFavItemList] = useState([]);
 
+    useEffect(() => {
+        // Check if localStorage is available and then set the state
+        const favItems = localStorage.getItem("favItemArr");
+        if (favItems) {
+            setFavItemList(JSON.parse(favItems));
+        }
+    }, []);
     return (
         <>
             <nav className='bg-[var(--primary)]  flex justify-between p-2 text-2xl'>
@@ -13,7 +20,7 @@ const page = () => {
                 <Menu />
             </nav>
             <main>
-                {(favItemList) ? favItemList.map((item) => {
+                {(favItemList.length>0) ? favItemList.map((item) => {
                     console.log(item.mealObj);
                     return <RecipeCard mealObj={item.mealObj} />
                 }) : console.log("No data")}
